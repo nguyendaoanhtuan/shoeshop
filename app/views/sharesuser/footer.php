6e60d9d4029742d1b43131424c8032437a2f6c74
Nguyễn Đào Anh Tuấn <156777172+nguyendaoanhtuan@users.noreply.github.com>
2025-03-31 13:14:48 +0700

    Update footer.php

--- app/views/sharesuser/footer.php
+++ app/views/sharesuser/footer.php
@@ -4,51 +4,32 @@
         <div class="row">
             <div class="col-lg-3  col-md-6 col-sm-6">
                 <div class="single-footer-widget">
-                    <h6>Về chúng tôi</h6>
-                    <p>
-                        ShoeShop - Cửa hàng giày uy tín hàng đầu Việt Nam
-                    </p>
-                </div>
-            </div>
-            <div class="col-lg-4  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Đăng ký nhận tin</h6>
-                    <p>Nhận thông tin về sản phẩm mới và khuyến mãi</p>
-                    <div class="" id="mc_embed_signup">
-                        <form target="_blank" novalidate="true" action="#" method="get" class="form-inline">
-                            <div class="d-flex flex-row">
-                                <input class="form-control" name="EMAIL" placeholder="Email" required="" type="email">
-                                <button class="click-btn btn btn-default"><span class="ti-arrow-right"></span></button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3  col-md-6 col-sm-6">
-                <div class="single-footer-widget">
-                    <h6>Theo dõi chúng tôi</h6>
+                    <h6>Theo dõi chúng tôi trên các nền tảng</h6>
+                    <p></p>
                     <div class="footer-social d-flex align-items-center">
                         <a href="#"><i class="fa fa-facebook"></i></a>
                         <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-dribbble"></i></a>
+                        <a href="#"><i class="fa fa-behance"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4  col-md-6 col-sm-6">
+            </div>
+            <div class="col-lg-3  col-md-6 col-sm-6">
+                <div class="single-footer-widget mail-chimp">
+                    <div class="single-footer-widget">
+                        <a href="../../../contact.html"><h6>Về chúng tôi</h6></a>
+                        <p>Số điện thoại: 0789123456</p>
+                        <p>Email: support@shoeshop.com</p>
+                        <p>Địa chỉ: khu phố 6, phường Linh Trung, quận Thủ Đức, thành phố Hồ Chí Minh</p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-2 col-md-6 col-sm-6">
                 <div class="single-footer-widget">
-                    <h6>Liên hệ</h6>
-                    <p>
-                        <strong>Địa chỉ:</strong> 123 ABC Street<br>
-                        <strong>Email:</strong> info@shoeshop.com<br>
-                        <strong>Phone:</strong> +84 123 456 789
-                    </p>
                 </div>
             </div>
-        </div>
-        <div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
-            <p class="footer-text m-0">
-                Copyright &copy; <?php echo date('Y'); ?> All rights reserved | ShoeShop
-            </p>
         </div>
     </div>
 </footer>
